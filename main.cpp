da275fd36bbf5ce59a4c4d2adfb5d2cbcb119b54
Kacper <ciaparino123@gmail.com>
2024-07-25 19:50:18 +0200

    Pointers and arrays

--- main.cpp
+++ main.cpp
@@ -3,36 +3,26 @@
 using namespace std;
 //introduction to pointers ...
 
-void printNumber(int*numberPtr) {
-
-cout<<*numberPtr<<endl;
-
-}
-
-void printletter(char*charPtr) {
-
-    cout<<charPtr<<endl;
-
-}
-
-void print(void*ptr,char type) {
-
-    switch (type) {
-
-        case 'i':cout<<*((int*)ptr)<<endl;break;
-        case 'c':cout<<*((char*)ptr)<<endl;break;
-    }
-}
 
 int main() {
 
-int number=5;
-    char letter='a';
-    //printNumber(&number);
-    //printletter(&letter);
+int luckyNumbers[5];
 
-print(&number,'i');
-    print(&letter,'c');
+    for(int i=0;i<=4;i++) {
+
+        cout<<" Number: ";
+        cin>>luckyNumbers[i];
+
+    }
+
+    for(int i=0;i<=5;i++) {
+
+        cout<<"  ";
+        cout<<*(luckyNumbers+i)<<"  ";
+
+    }
+
+
 
     return 0;
 }
